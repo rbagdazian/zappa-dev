6617b1a3701f6bdc7fd870314546dba4074c7795
Richard Bagdazian <rbagdazian@gmail.com>
2021-03-15 18:54:43 +0000

    capture of grinberg tutorial step 1

---
+++ scripts/setupGit.sh
@@ -0,0 +1,4 @@
+git config --global user.name "Richard Bagdazian"
+git config --global user.email "rbagdazian@gmail.com"
+#git remote add origin git@github.com:rbagdazian/zappa-dev.git
+git remote set-url git@github.com:rbagdazian/zappa-dev.git
