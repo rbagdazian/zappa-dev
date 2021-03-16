77da86508f0f371fe318107c9b2c6643d228c101
Richard Bagdazian <rbagdazian@gmail.com>
2021-03-16 18:07:49 +0000

    end of database interface chapter

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,12 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
-from flask import render_template, flash, redirect, url_for
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Rich'}
+    user = {'username': 'Miguel'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -17,9 +16,9 @@ def index():
             'author': {'username': 'Susan'},
             'body': 'The Avengers movie was so cool!'
         }
-    ]    
+    ]
     return render_template('index.html', title='Home', user=user, posts=posts)
-    
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -28,4 +27,4 @@ def login():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)   
+    return render_template('login.html',  title='Sign In', form=form)
